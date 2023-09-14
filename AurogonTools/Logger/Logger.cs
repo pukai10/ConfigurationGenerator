2774945c6be4708dc693924dfc5f46a3ce861a69
Patrick <654062327@qq.com>
2023-09-14 15:48:42 +0800

    日志写入文件

--- AurogonTools/Logger/Logger.cs
+++ AurogonTools/Logger/Logger.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace AurogonTools
 {
@@ -8,8 +10,11 @@ namespace AurogonTools
 
     public class Logger : ILogger, IRegisterLogCallback
     {
+        #region 静态
+
         private static Dictionary<string, ILogger> m_loggers = new Dictionary<string, ILogger>();
         private static ILogger m_defaultLog = new Logger();
+        public static ILogger Default => m_defaultLog;
 
         public static ILogger GetLogger()
         {
@@ -18,35 +23,68 @@ namespace AurogonTools
 
         public static ILogger GetLogger(string logTag)
         {
+            return GetLogger(logTag, new LoggerSetting());
+        }
+
+        public static ILogger GetLogger(Type type)
+        {
+            return type == null ? GetLogger() : GetLogger(nameof(type), new LoggerSetting());
+        }
+
+        public static ILogger GetLogger(string logTag,LoggerSetting setting)
+        {
             if(m_loggers.TryGetValue(logTag,out ILogger logger))
             {
                 return logger;
             }
 
-            ILogger newLogger = new Logger(logTag);
+            ILogger newLogger = new Logger(logTag, setting);
             m_loggers.Add(logTag, newLogger);
 
             return newLogger;
         }
 
-        public static ILogger GetLogger(Type type)
+        public static ILogger GetLogger(Type type, LoggerSetting setting)
         {
-
-            return type == null ? GetLogger() : GetLogger(nameof(type));
+            return type == null ? GetLogger() : GetLogger(nameof(type),setting);
         }
 
+        #endregion
+
+        #region 成员变量
 
         private string m_logTag = string.Empty;
         public LoggerSetting loggerSetting { get; set; }
         private event LogCallback m_logCallback = null;
-
-        public Logger()
+        private FileInfo m_fileInfo = null;
+        private StreamWriter m_streamWriter = null;
+        private string m_logFilePath;
+        public string logFilePath
         {
+            get
+            {
+                if (string.IsNullOrEmpty(m_logFilePath))
+                {
+                    m_logFilePath = $"{loggerSetting.logPath}\\{m_logTag}{DateTime.Now.ToString("yyyy_MM_dd")}.log";
+                }
 
+                return m_logFilePath;
+            }
         }
-        public Logger(string logTag)
+
+        #endregion
+
+        public Logger():this(string.Empty,new LoggerSetting())
+        {
+        }
+
+        public Logger(string logTag,LoggerSetting setting)
         {
             m_logTag = logTag;
+            loggerSetting = setting;
+
+            m_fileInfo = new FileInfo(logFilePath);
+            ClearAndReCreateFile();
         }
 
         public void Debug(string content)
@@ -86,11 +124,11 @@ namespace AurogonTools
 
             content = string.Format("{0} {1,-10} {2}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), logType, content);
             ConsoleLog(content, logType, stackTrace);
+            WriteToFile(content, logType, stackTrace);
             if (m_logCallback != null)
             {
                 m_logCallback(content, logType, stackTrace);
             }
-
         }
 
         private void ConsoleLog(string content, LogType logType, string stackTrace)
@@ -126,6 +164,38 @@ namespace AurogonTools
 
             Console.ForegroundColor = oldColor;
 
+        }
+
+        private void WriteToFile(string content, LogType logType, string stackTrace)
+        {
+
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine(content);
+                sb.AppendLine(stackTrace);
+                m_streamWriter = m_fileInfo.AppendText();
+                m_streamWriter.Write(sb.ToString());
+                m_streamWriter.Close();
+                m_streamWriter.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void ClearAndReCreateFile()
+        {
+            if(m_fileInfo.Exists)
+            {
+                m_fileInfo.Delete();
+            }
+
+            m_streamWriter = m_fileInfo.CreateText();
+            m_streamWriter.WriteLine($"=========={DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} {m_logTag} Log Start==========");
+            m_streamWriter.Close();
+            m_streamWriter.Dispose();
         }
 
         private void InitLogSetting()
